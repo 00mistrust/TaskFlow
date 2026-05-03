1785b353a3c33e3e5338442a07c7fe60bb812000
Marwa EL OUAAI <Marwa.elouaai@etu.uae.ac.ma>
2026-05-03 20:36:10 +0100

    feat: configurer serveur Express et connexion MongoDB

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const taskRoutes = require('./routes/tasks');
+
+const app = express();
+app.use(express.json());
+
+mongoose.connect('mongodb://localhost:27017/taskflow', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => console.log('MongoDB connecté'))
+  .catch(err => console.error(err));
+
+app.use(taskRoutes);
+
+app.listen(3000, () => {
+  console.log('Serveur démarré sur http://localhost:3000');
+});
