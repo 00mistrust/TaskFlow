f1675825cae119c582342564decf048580d07be9
MR12006 <MR12006>
2026-05-04 15:25:11 +0100

    Fix error of User edition

--- backend/src/controllers/authController.js
+++ backend/src/controllers/authController.js
@@ -1,11 +1,11 @@
-const user = require('../models/user');
+const User = require('../models/User');
 const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 exports.register = async (req, res) => {
   try {
     const { nom, email, motDePasse } = req.body;
-    const user = await user.create({ nom, email, motDePasse });
+    const user = await User.create({ nom, email, motDePasse });
     res.status(201).json({ message: 'Compte créé avec succès' });
   } catch (err) {
     res.status(400).json({ error: err.message });
