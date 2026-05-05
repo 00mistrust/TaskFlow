e3ef03f29fb1f1e1eb0c36e3d98b2a0dcf36d97d
MR12006 <MR12006>
2026-05-05 14:33:38 +0100

    feat: add dashboard route to server

--- backend/server.js
+++ backend/server.js
@@ -15,8 +15,8 @@ mongoose.connect(process.env.MONGO_URI)
 
 app.use('/api/auth', authRoutes);
 // app.use('/api/tasks', require('./src/routes/tasks'));
-
-app.get('/', (req, res) => res.json({ message: 'amma Nice Job Team time to show up !' }));
+app.use('/api/dashboard', require('./src/routes/dashboard'));
+app.get('/', (req, res) => res.json({ message: 'Nice Job Team, pround of you ALL, now time to show up !' }));
 
 app.listen(process.env.PORT, () => {
   console.log(`Serveur lancé sur le port ${process.env.PORT}`);
