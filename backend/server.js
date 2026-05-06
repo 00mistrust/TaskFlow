081fe6c795eeeea214e1071251ba7976f396dd9e
MR12006 <MR12006>
2026-05-06 22:57:22 +0100

    fix the reposotary problem in server

--- backend/server.js
+++ backend/server.js
@@ -16,9 +16,8 @@ mongoose.connect(process.env.MONGO_URI)
 app.use('/api/auth', authRoutes);
 app.use('/api/tasks', require('./src/routes/tasks'));
 app.use('/api/dashboard', require('./src/routes/dashboard'));
+app.use('/api/projects', require('./src/routes/projects'));
 app.get('/', (req, res) => res.json({ message: 'Nice Job Team, pround of you ALL, now time to show up !' }));
-const projectRoutes = require('./backend/src/routes/projects');
-app.use('/api/projects', projectRoutes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Serveur lancé sur le port ${process.env.PORT}`);
