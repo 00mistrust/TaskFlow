7db0264c492957b010abeedcc1f0e4c912278bdb
Marwa EL OUAAI <Marwa.elouaai@etu.uae.ac.ma>
2026-05-16 00:03:52 +0100

    feat : afficher popup etes vous sur de sedeconnecter

--- frontend/js/navbar.js
+++ frontend/js/navbar.js
@@ -12,3 +12,17 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log("failed to change name f navbar");
   }
 });
+// Gérer le clic sur le bouton de déconnexion avec confirmation
+const logoutBtn = document.getElementById('logoutBtn');
+if (logoutBtn) {
+  logoutBtn.addEventListener('click', (e) => {
+    e.preventDefault(); 
+    const reponse = confirm("Êtes-vous sûr de vouloir vous déconnecter ?");
+    
+    if (reponse) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('userName'); 
+      window.location.href = 'login.html'; 
+    }
+  });
+}
