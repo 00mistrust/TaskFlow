44b50d0985b2afa2a577b45c31b6ff3d746cb544
MR12006 <MR12006>
2026-05-18 01:17:28 +0100

    add notification tool to our project

--- backend/server.js
+++ backend/server.js
@@ -20,6 +20,7 @@ app.use('/api/auth', authRoutes);
 app.use('/api/tasks', require('./src/routes/tasks'));
 app.use('/api/dashboard', require('./src/routes/dashboard'));
 app.use('/api/projects', require('./src/routes/projects'));
+app.use('/api/notifications', require('./src/routes/notifications'));
 
 app.get('/', (req, res) => res.json({ message: 'Nice Job Team, pround of you ALL, now time to show up !' }));
 
